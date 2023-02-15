ea89bc529de4f5f26b2a8a0723638ee725d23e5d
zm1060 <zm1575098153@gmail.com>
2023-02-15 22:54:38 +0800

    add new crawler: ChinaNews

---
+++ crawler/ChinaNews/ChinaNews/spiders/chinanews.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class ChinanewsSpider(scrapy.Spider):
+    name = 'chinanews'
+    allowed_domains = ['www.chinanews.com']
+    start_urls = ['http://www.chinanews.com/']
+
+    def parse(self, response):
+        pass

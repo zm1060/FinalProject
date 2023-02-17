3730e8056a39033efcbff1782dc68300f7c791d3
zm1060 <zm1575098153@gmail.com>
2023-02-17 19:25:04 +0800

    集成scrapydweb!!!

--- crawler/ChinaNews/run_china_news.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_scrapy_spider():
-    spider_name = 'chinanews'
-    cmd = ['scrapy', 'crawl', spider_name]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = proc.communicate()
-    return out.decode('utf-8'), err.decode('utf-8')

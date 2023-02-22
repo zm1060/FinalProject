fc1c60bf4e9299ca6606c24eb15b55b048e7f75c
zm1060 <zm1575098153@gmail.com>
2023-02-23 00:22:17 +0800

    prepare for integrated celery

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,38 @@
 import uvicorn
-from fastapi import BackgroundTasks, Depends, FastAPI
+from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException
+
 
 from model.User import User
 from model.Task import Task
 
 app = FastAPI()
+
+
+
+def send_email(email, message):
+    pass
+
+@app.get("/")
+async def ping(background_tasks: BackgroundTasks):
+    background_tasks.add_task(send_email, "email@address.com", "Hi!")
+    return {"message": "pong!"}
+
+
+# Define API endpoints
+@app.post("/spiders")
+async def create_spider(url: str):
+    task = crawl.delay(url)
+    return {"task_id": task.id}
+
+@app.get("/spiders/{task_id}")
+async def get_spider(task_id: str):
+    task = celery_app.AsyncResult(task_id)
+    if task.state == "SUCCESS":
+        return {"status": "completed", "result": task.result}
+    elif task.state == "PENDING":
+        raise HTTPException(status_code=404, detail="Task not found")
+    else:
+        raise HTTPException(status_code=500, detail="Task failed")
 
 @app.post("/task/create")
 async def create_task(task: Task):
